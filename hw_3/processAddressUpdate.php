8c09bdab55e1080a5c40d708115abeefd2790416
Thomas Ngo <tngo0508@gmail.com>
2018-03-06 23:23:40 -0800

    clean code

--- hw_3/processAddressUpdate.php
+++ hw_3/processAddressUpdate.php
@@ -11,40 +11,41 @@ $country = (string) trim(preg_replace("/\t|\R/",' ',$_POST['country']));
 require_once('Address.php');
 
 $name = "$last_name".', '."$first_name";
-if (!($last_name == "")) {
-  if ($first_name == "") {
-    $first_name = null;
-  }
-  if ($street == "") {
-    $street = null;
-  }
-  if ($city == "") {
-    $city = null;
-  }
-  if ($state == "") {
-    $state = null;
-  }
-  if ($zipCode == 0) {
-    $zipCode = null;
-  }
-  if ($country == "") {
-    $country = null;
-  }
-  @$db = new mysqli('localhost', 'thomas', 'me123', 'CPSC_431_HW3');
-
-  if (mysqli_connect_errno()) {
-    echo "<p>Error: Could not connects to database.<br/>
-          Please try again later.</p>";
-    exit;
-  }
-
-  $query = "INSERT INTO TeamRoster (Name_First, Name_Last, Street, City, State, Country, ZipCode) VALUES (?, ?, ?, ?, ?, ?, ?)";
-  $stmt = $db->prepare($query);
-  $stmt->bind_param('sssssss', $first_name, $last_name, $street, $city, $state, $country, $zipCode);
-  $stmt->execute();
-
-  $db->close();
+if ($last_name == "") {
+  $last_name = null;
 }
+if ($first_name == "") {
+  $first_name = null;
+}
+if ($street == "") {
+  $street = null;
+}
+if ($city == "") {
+  $city = null;
+}
+if ($state == "") {
+  $state = null;
+}
+if ($zipCode == 0) {
+  $zipCode = null;
+}
+if ($country == "") {
+  $country = null;
+}
+@$db = new mysqli('localhost', 'thomas', 'me123', 'CPSC_431_HW3');
+
+if (mysqli_connect_errno()) {
+  echo "<p>Error: Could not connects to database.<br/>
+        Please try again later.</p>";
+  exit;
+}
+
+$query = "INSERT INTO TeamRoster (Name_First, Name_Last, Street, City, State, Country, ZipCode) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$stmt = $db->prepare($query);
+$stmt->bind_param('sssssss', $first_name, $last_name, $street, $city, $state, $country, $zipCode);
+$stmt->execute();
+
+$db->close();
 
 require_once('home_page.php');
 
