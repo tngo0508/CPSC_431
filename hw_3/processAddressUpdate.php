bfc98cb7457869c0b126cf6484a4594df12aaf7a
Thomas Ngo <tngo0508@gmail.com>
2018-03-06 22:36:18 -0800

    fix empty string in Last Name

--- hw_3/processAddressUpdate.php
+++ hw_3/processAddressUpdate.php
@@ -11,22 +11,25 @@ $country = (string) trim(preg_replace("/\t|\R/",' ',$_POST['country']));
 $name = "$last_name".', '."$first_name";
 require_once('Address.php');
 
-$newPlayer = new Address($name, $street, $city, $state, $country, $zipCode);
+if (!($last_name == "")) {
+  $newPlayer = new Address($name, $street, $city, $state, $country, $zipCode);
 
-@$db = new mysqli('localhost', 'thomas', 'me123', 'CPSC_431_HW3');
+  @$db = new mysqli('localhost', 'thomas', 'me123', 'CPSC_431_HW3');
 
-if (mysqli_connect_errno()) {
-  echo "<p>Error: Could not connects to database.<br/>
-        Please try again later.</p>";
-  exit;
+  if (mysqli_connect_errno()) {
+    echo "<p>Error: Could not connects to database.<br/>
+          Please try again later.</p>";
+    exit;
+  }
+
+  $query = "INSERT INTO TeamRoster (Name_First, Name_Last, Street, City, State, Country, ZipCode) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  $stmt = $db->prepare($query);
+  $stmt->bind_param('ssssssd', $first_name, $last_name, $street, $city, $state, $country, $zipCode);
+  $stmt->execute();
+
+  $db->close();
 }
 
-$query = "INSERT INTO TeamRoster (Name_First, Name_Last, Street, City, State, Country, ZipCode) VALUES (?, ?, ?, ?, ?, ?, ?)";
-$stmt = $db->prepare($query);
-$stmt->bind_param('ssssssd', $first_name, $last_name, $street, $city, $state, $country, $zipCode);
-$stmt->execute();
-
-$db->close();
-
 require_once('home_page.php');
+
  ?>
