18860bc568634902414d62ce468bd88daee1799e
Thomas Ngo <tngo0508@gmail.com>
2018-03-20 08:29:29 -0700

    upload hw_3 solution

---
+++ hw_3/HW-3 Solution/Adaptation.php
@@ -0,0 +1,6 @@
+<?php
+  define('DATA_BASE_NAME', 'CSPS_431_HW3');
+  define('DATA_BASE_HOST', 'localhost');
+  define('USER_NAME',      'phpWebEngine');
+  define('USER_PASSWORD',  'withheld');
+?> 
